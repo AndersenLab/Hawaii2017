72bd858f6176a6f4edddc841e23fb487d8169d4e
danielecook <danielecook@gmail.com>
2017-04-12 10:59:46 -0500

    add labels

--- labels/generate_labels.py
+++ labels/generate_labels.py
@@ -1,32 +1,90 @@
 import labels
 from reportlab.graphics import shapes
+from reportlab.graphics.barcode import eanbc
 import qrcode
 from tempfile import mktemp
 
-# Create an A4 portrait (210mm x 297mm) sheets with 2 columns and 8 rows of
-# labels. Each label is 90mm x 25mm with a 2mm rounded corner. The margins are
-# automatically calculated.
-specs = labels.Specification(210, 297, 2, 8, 90, 25, corner_radius=2)
 
-# Create a function to draw each label. This will be given the ReportLab drawing
-# object to draw on, the dimensions (NB. these will be in points, the unit
-# ReportLab uses) of the label, and the object to render.
+people = ['DEC', 'SCB', 'SXZ', 'ECA', 'KSE', 'DDL', 'BCR']
+
+label_types = {
+    "a_5963": labels.Specification(210, 297, 2, 5, 102, 54, corner_radius=2,
+                                   top_margin=8,
+                                   row_gap=2,
+                                   left_margin=0,
+                                   right_margin=0),
+    "a_5267": labels.Specification(215.90, 297.40, 4, 20, 47, 14, corner_radius=2,
+                                   top_margin=8,
+                                   row_gap=0,
+                                   left_margin=0,
+                                   right_margin=0),
+
+}
+
+#===================#
+# Collection Labels #
+#===================#
+
 def draw_label(label, width, height, obj):
     # Generate QR Code for labels
     qr_barcode = mktemp(suffix='.png')
     qrcode.make(str(obj['ID'])).save(qr_barcode, "png")
-    label.add(shapes.Image(0, 0, 70, 70, qr_barcode))
-    # Add ID
-    label.add(shapes.String(70, 50, str(obj['ID']), fontName="Helvetica", fontSize=15))
-    label.add
+    label.add(shapes.Image(0, 80, 70, 70, qr_barcode))
+    # Add ID, date, time
+    label.add(shapes.String(70, 128, str(
+        obj['ID']), fontName="Courier", fontSize=18))
+    label.add(shapes.String(70, 100, str("08 / __ / 17"),
+                            fontName="Courier", fontSize=14))
+    label.add(shapes.String(200, 100, str("___:___"),
+                            fontName="Courier", fontSize=14))
 
-# Create the sheet.
-sheet = labels.Sheet(specs, draw_label, border=True)
+    label.add(shapes.String(10, 60, str(
+        "Lat;Lon    __________;__________"), fontName="Courier", fontSize=14))
 
-# Add a couple of labels.
-sheet.add_label({"ID":"Identifier #1"})
-sheet.add_label({"ID":"Identifier #2"})
+    # Substrate
+    label.add(shapes.String(10, 40, str(
+        "Substrate  __________________"), fontName="Courier", fontSize=14))
 
-# Save the file and we are done.
-sheet.save('basic.pdf')
-print("{0:d} label(s) output on {1:d} page(s).".format(sheet.label_count, sheet.page_count))
+    # Humidity
+    label.add(shapes.String(10, 20, str("Humidity   _____%"),
+                            fontName="Courier", fontSize=14))
+
+specs = label_types['a_5963']
+sheet = labels.Sheet(specs, draw_label, border=False)
+
+# Generate large labels
+for person in people:
+    for i in range(0, 10):
+        _id = person + "-" + str("%04i" % (i + 1))
+        sheet.add_label({"ID": _id})
+sheet.save('collection-forms.pdf')
+print("{0:d} label(s) output on {1:d} page(s).".format(
+    sheet.label_count, sheet.page_count))
+
+
+#====================#
+# Plating Out Labels #
+#====================#
+
+def draw_label_plate(label, width, height, obj):
+    # Generate QR Code for labels
+    qr_barcode = mktemp(suffix='.png')
+
+    qrcode.make(str(obj['ID'])).save(qr_barcode, "png")
+    label.add(shapes.Image(3, -2, 44, 44, qr_barcode))
+    label.add(shapes.String(45, 16, str(
+        obj['ID']), fontName="Courier", fontSize=16))
+
+
+specs = label_types['a_5267']
+sheet = labels.Sheet(specs, draw_label_plate, border=False)
+
+# Generate large labels
+for person in people:
+    for i in range(0, 10):
+        _id = person + "-" + str("%04i" % (i + 1))
+        sheet.add_label({"ID": _id})
+
+sheet.save('plating-labels.pdf')
+print("{0:d} label(s) output on {1:d} page(s).".format(
+    sheet.label_count, sheet.page_count))
